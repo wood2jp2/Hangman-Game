1ea31a8eb91b98a13fb4fa092305d335f0a9b2ba
Josh Wood <wood2jp2@gmail.com>
2017-05-23 23:53:12 -0400

    relatively functioning game, need to figure out original puzzle spacings and guess counter

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,13 +1,27 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var lettersGuessed = ["Letters guessed: "];
+var lettersGuessed = [];
 var arrayOfPuzzles = ["bitcoin", "whatsapp", "google", "facebook", "amazon", "techcrunch", "twitter", "startup", "fullstack"]
+var puzzleChoice = arrayOfPuzzles[Math.floor(Math.random()*arrayOfPuzzles.length)];
+var solverArray = [];
 
 document.onkeyup = function(event) {
-	var puzzleChoice = arrayOfPuzzles[Math.floor(Math.random()*arrayOfPuzzles.length)]
 	var userGuess = event.key;
-	lettersGuessed[lettersGuessed.length] = userGuess;
+	lettersGuessed.push(userGuess);
 
-
+	for (var i = 0; i<puzzleChoice.length; i++) {
+		var j = puzzleChoice[i];
+		if (userGuess===j) {
+			solverArray[i]=userGuess;}
+		//  } else if (solverArray[i].isN{
+		// // 	solverArray[i] = "_";
+		// // }
+	}
+	guessesLeft-=1;
+	document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("userGuesses").innerHTML = lettersGuessed;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    document.getElementById("puzzle").innerHTML = solverArray;
 }
